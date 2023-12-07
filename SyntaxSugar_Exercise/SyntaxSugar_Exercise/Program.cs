475bf7cb93e489d3cc7dbffbea56ccfa34cc5a7e
elijah jones <jones.elijah4679@gmail.com>
2023-12-07 17:47:25 -0600

    first

---
+++ SyntaxSugar_Exercise/SyntaxSugar_Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SyntaxSugar_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given the following code, change the code to implement
+            // a.) inferred typing b.) string interpolation c.) Ternary Operators
+            /*int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+            Console.WriteLine(response);*/
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+        }
+    }
+}
